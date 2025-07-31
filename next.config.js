c4eb518e563e8245777c2ba47ea660dceccac3df
mscreationweb <mscreation361@gmail.com>
2025-07-31 15:45:40 +0530

    test frontend 7

--- next.config.js
+++ next.config.js
@@ -1,10 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  images: {
+     output: 'export',
+       images: {
     unoptimized: true,
   },
-  trailingSlash: true, // Important to avoid 404s
 }
 
 module.exports = nextConfig
